124d3550719d376225acef40180ceedd5186dda7
Michael Hvid Olesen <hvid91@yahoo.dk>
2019-12-18 17:15:00 +0100

    Er i gang med at prøve at hente en besemt bog ud fra kun id på bog

--- client/src/SellBook.js
+++ client/src/SellBook.js
@@ -36,25 +36,27 @@ export default class SellBook extends Component {
         return (
             <form>
                 <div className="field">
-                    <label className="label" htmlFor="QuestionInput">The title</label>
+                    <label className="label" >The title</label>
                     <input onChange={event => this.onChange(event)} name="title"/>
-                    <label className="label" htmlFor="QuestionInput">The author</label>
+                    <label className="label" >The author</label>
                     <input onChange={event => this.onChange(event)} name="author"/>
-                    <label className="label" htmlFor="QuestionInput">The category</label>
+                    <label className="label" >The category</label>
                     <select onChange={event => this.onChange(event)}
-                            name="categoryID">{this.props.categories.map((c, index) =>
+                            name="categoryID">
+                        <option value="" selected disabled hidden>Choose here</option>
+                        {this.props.categories.map((c, index) =>
                         <option key={index} value={c._id}>{c.category}</option>)}
                     </select>
-                    <label className="label" htmlFor="QuestionInput">The price</label>
+                    <label className="label" >The price</label>
                     <input onChange={event => this.onChange(event)} type="number" name="price"/>
-                    <label className="label" htmlFor="QuestionInput">Your name</label>
+                    <label className="label" >Your name</label>
                     <input onChange={event => this.onChange(event)} name="sellerName"/>
-                    <label className="label" htmlFor="QuestionInput">Your email</label>
+                    <label className="label" >Your email</label>
                     <input onChange={event => this.onChange(event)} name="sellerEmail"/>
                 </div>
                 <div className="field">
                     <button className="button is-primary" onClick={this.handleInput} type="submit"
-                            id="QuestionButton">Create user
+                            id="BookButton">Sell book
                     </button>
                 </div>
             </form>
